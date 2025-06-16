c2671c1721c02e526af0082f43e00f72c94e47c0
Tino Tonitini <georgepike0@gmail.com>
2025-06-16 18:03:26 -0400

    Fix helix view test import order

--- tests/test_helix_view.py
+++ tests/test_helix_view.py
@@ -4,10 +4,10 @@ ft = pytest.importorskip("flet")
 if not hasattr(ft, "HtmlElement"):
     pytest.skip("Flet HtmlElement not available", allow_module_level=True)
 
-from genecoder.helix_view import show_helix
-
 
 def test_show_helix_basic():
+    from genecoder.helix_view import show_helix
+
     elem = show_helix()
     assert isinstance(elem, ft.HtmlElement)
     assert elem.width == 600
