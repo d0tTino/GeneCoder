c2c32af6ef8f4038d30b9ccbb0e00c0ab63a01d8
Tino Tonitini <georgepike0@gmail.com>
2025-06-17 11:10:44 -0400

    Specify supported Flet version

--- src/genecoder/helix_view.py
+++ src/genecoder/helix_view.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import flet as ft
+from urllib.parse import quote
 
 HELIX_HTML = """
 <div id='helix-container' style='width:100%; height:100%;'></div>
@@ -35,6 +36,7 @@ animate();
 </script>
 """
 
-def show_helix() -> ft.HtmlElement:
-    """Return an ``HtmlElement`` displaying a basic DNA helix scene."""
-    return ft.HtmlElement(content=HELIX_HTML, width=600, height=400)
+def show_helix() -> ft.WebView:
+    """Return a ``WebView`` displaying a basic DNA helix scene."""
+    data_url = "data:text/html," + quote(HELIX_HTML)
+    return ft.WebView(url=data_url, width=600, height=400)
--- tests/test_helix_view.py
+++ tests/test_helix_view.py
@@ -1,4 +1,3 @@
-import pytest
 
 
 
@@ -6,7 +5,7 @@ def test_show_helix_basic():
     from genecoder.helix_view import show_helix
 
     elem = show_helix()
-    assert isinstance(elem, ft.HtmlElement)
+    assert elem.__class__.__name__ == "WebView"
     assert elem.width == 600
     assert elem.height == 400
-    assert "helix-container" in elem.content
+    assert elem.url.startswith("data:text/html,")
